4c619ea0398e3e1bae0bf3fc59fa88f26ec4bb5d
Alexandre-Demling <alexandredemling@gmail.com>
2025-03-13 14:04:49 +0100

    netlify
    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
-    experimental: { svg: true },
+  integrations: [tailwind()],
+  experimental: { svg: true },
+  adapter: netlify(),
+  output: 'server',
 });
