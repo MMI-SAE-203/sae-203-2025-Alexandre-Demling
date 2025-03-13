f64f663043e58c3d5d1e072a35a9f97a2c2c2692
Alexandre-Demling <alexandredemling@gmail.com>
2025-03-13 17:14:09 +0100

    Vrai bon local pb tout site local

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-const pb = new PocketBase('https://sae203.alexandre-demling.fr');
+const pb = new PocketBase('http://127.0.0.1:8090');
 
 export async function dateFilm() {
     const records = await pb.collection('film').getFullList({ sort: 'Date_projection_film', });
